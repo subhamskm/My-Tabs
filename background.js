b9756eec56699525a756f8a88d296417fcbe7db3
Subham Kumar <shubham.kumar.ggps@gmail.com>
2019-05-09 00:29:40 +0530

    second commit

--- background.js
+++ background.js
@@ -15,14 +15,3 @@ chrome.browserAction.onClicked.addListener(function(tab) {
           }
         }
   });
-
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-      /*if( request.message === "open_new_tab" ) {
-        chrome.tabs.create({"url": request.url});
-      }
-      else if( request.message === "open_new_tab" ) {*/
-        chrome.tabs.create({"url": request.url});
-      //}
-    }
-  );
--- content.js
+++ content.js
@@ -1,18 +1,1 @@
 // content.js
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-      if( request.message === "clicked_browser_action" ) {
-        var hrefs = ["https://www.google.com/", "https://www.facebook.com"];
-  
-        for(i=0; i<hrefs.length; i++){
-          console.log(hrefs[i]);
-          /*if(i==0){
-            chrome.runtime.sendMessage({"message": "update_current_tab", "url": hrefs[i]});
-          }
-          else{*/
-            chrome.runtime.sendMessage({"message": "open_new_tab", "url": hrefs[i]});
-          //}
-        }
-      }
-    }
-  );
