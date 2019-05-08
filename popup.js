57e6b6d72cb0f5d23e8f7e2fa4a30d24120f9f96
Subham Kumar <shubham.kumar.ggps@gmail.com>
2019-05-09 02:16:30 +0530

    fourth commit

--- popup.js
+++ popup.js
@@ -1,26 +1,35 @@
-let changeColor = document.getElementById('opentab');
+let opentabs = document.getElementById('opentab');
 
-/*chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
-});*/
-
-changeColor.onclick = function (element) {
-    /*let color = element.target.value;
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.executeScript(
-          tabs[0].id,
-          {code: 'document.body.style.backgroundColor = "' + color + '";'});
-    });*/
+opentabs.onclick = function (element) {
+    console.log("clicked");
     var hrefs = ["https://www.google.com/", "https://www.facebook.com", "https://www.youtube.com/", "https://www.gmail.com"];
 
-    for (i = 0; i < hrefs.length; i++) {
-        console.log(hrefs[i]);
-        if (i == 0) {
-            chrome.tabs.update({ "url": hrefs[i] });
+    chrome.storage.sync.get({ urls: [] }, function (result) {
+        var hrefs = result.urls;
+        for (i = 0; i < hrefs.length; i++) {
+            if (i == 0) {
+                chrome.tabs.update({ "url": hrefs[i] });
+            }
+            else {
+                chrome.tabs.create({ "url": hrefs[i] });
+            }
         }
-        else {
-            chrome.tabs.create({ "url": hrefs[i] });
-        }
-    }
+    });
+};
+
+let savetab = document.getElementById('savetab');
+
+savetab.onclick = function (element) {
+    chrome.storage.sync.get({ urls: [] }, function (result) {
+        var urls = result.urls;
+        chrome.tabs.query({ 'active': true, 'currentWindow': true }, function (tabs) {
+            var newurl = tabs[0].url;
+            urls.push(newurl);
+            chrome.storage.sync.set({ urls: urls }, function () {
+                chrome.storage.sync.get('urls', function (result) {
+                    console.log(result.urls);
+                });
+            });
+        });
+    });
 };
